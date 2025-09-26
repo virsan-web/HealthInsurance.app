536573b5ef9eb54ee2f2fa779aab92db0db3b895
virsan-web <virsanofficial23@gmail.com>
2025-09-26 17:59:32 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pickle
+st.title('Health Insurance Premium Prediction')
+age = st.number_input('Age:')
+bmi = st.number_input('BMI:')
+childer = st.number_input('Number of Children:')
+gender = st.text_input('Gender')
+smoker = st.text_input('Smoker (yes/no):')
+model = pickle.load(open('model.pkl', 'rb'))
+
+if st.button('Predict'):
+    gender = 0 if gender.upper() == 'MALE' else 1
+    smoker = 0 if smoker.upper() == 'NO' else 1
+    X_test = [[age, gender, bmi,childer, smoker]]
+    yp=str(round(model.predict(X_test)[0],2))
+    st.write('Your Premium is'+yp)
